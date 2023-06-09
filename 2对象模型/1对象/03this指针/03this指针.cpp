ef075475b66f14bec82b13900d5d8c82a5bd0328
xlin <linxianghs@163.com>
2023-06-09 18:35:21 +0800

    object

---
+++ 2对象模型/1对象/03this指针/03this指针.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class A {
+public:
+	int a;
+	A() {
+		printf("A::A() 的 this指针是：%p!\n", this);
+	}
+	void funA() { printf("A::funcA: this = %p\n", this); }
+
+};
+
+class B {
+public:
+	int b;
+	B() {
+		printf("B::B() 的 this指针是：%p!\n", this);
+	}
+	void funB() { printf("B::funcB: this = %p\n", this); }
+
+};
+
+class C : public A, public B {
+public:
+	int c;
+	C() {
+		printf("C::C() 的 this指针是：%p!\n", this);
+	}
+	void funC() { printf("C::funcC: this = %p\n", this); }
+	void funB() { printf("C::funcB: this = %p\n", this); } //定义在C类中
+};
+
+
+int main() {
+	cout << sizeof(A) << endl;
+	cout << sizeof(B) << endl;
+	cout << sizeof(C) << endl;
+	C myc;
+	myc.funA();
+	myc.funB();
+	myc.B::funB();
+	myc.funC();
+
+	return 0;
+
+}
