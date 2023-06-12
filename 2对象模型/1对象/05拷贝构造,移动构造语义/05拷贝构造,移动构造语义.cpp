215611e31943850895badc09e91ea0365fb467c7
xlin <linxianghs@163.com>
2023-06-12 10:11:47 +0800

    object

---
+++ 2对象模型/1对象/05拷贝构造,移动构造语义/05拷贝构造,移动构造语义.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//拷贝构造语义
+class ASon
+{
+public:
+	int m_testson;
+};
+
+//class A
+//{
+//public:
+//	int m_test;
+//};
+
+
+
+//class CTB
+//{
+//public:
+//	CTB(const CTB&) //拷贝构造函数
+//	{
+//		cout << "CTB类的拷贝构造函数执行了" << endl;
+//	}	
+//	CTB() //缺省构造函数
+//	{
+//	}
+//};
+//class CTBSon :public CTB
+//{
+//public:
+//};
+class CTB
+{
+public:
+	virtual void mvirfunc() {}
+};
+
+//
+//class CTBSon //:public CTB
+//{
+//public:
+//	virtual void mvirfunc() {}; //定义一个虚函数
+//};
+class CTBSon :public CTB
+{
+public:
+	//virtual void mvirfunc() {}
+};
+
+
+//class A
+//{
+//public:
+//	int m_test;
+//	ASon asubobj;
+//	CTB m_ctb;
+//};
+
+class Grand //爷爷类
+{
+public:
+};
+class A : virtual public  Grand
+{
+public:
+};
+class A2 : virtual  public  Grand
+{
+public:
+};
+class C :public A, public A2
+{
+public:
+};
+
+
+//移动构造语义
+struct TC
+{
+	int i;          //内置类型可以移动
+	std::string s;  //string类型定义了自己的移动操作
+	//~TC() {}
+};
+
+
+int main()
+{
+	//{
+	//	A mya1;
+	//	mya1.m_test = 15;
+	//	A mya2 = mya1;  //这属于拷贝构造动作，但编译器没有合成拷贝构造函数
+	//	cout << "断点设置在这里" << endl;
+	//}
+	/*{
+		A mya1;
+		mya1.m_test = 15;
+		mya1.asubobj.m_testson = 120;
+		A mya2 = mya1;
+		cout << "断点设置在这里" << endl;
+	}*/
+	/*{
+		CTBSon myctbson1;
+		CTBSon myctbson2 = myctbson1;
+
+	}*/
+
+	//{
+	//	C cc;
+	//	C cc2 = cc;
+
+	//}
+	TC a;
+	a.i = 100;
+	a.s = "I Love China!";
+	const char* p = a.s.c_str();
+	TC b = std::move(a);  //导致结构/类TC移动构造函数的执行,数据移动不是std::move所为，而是string的移动构造函数所为
+	const char* q = b.s.c_str();
+
+
+	return 0;
+
+}
---
+++ 2对象模型/1对象/06程序转换语义/06程序转换语义.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class X {
+public:
+	int m_i;
+	X(const X& tmpx){
+		m_i = tmpx.m_i;
+		cout << "X类的拷贝构造函数被调用" << endl;
+	}
+	X(){
+		m_i = 0;
+		cout << "X类的构造函数被调用" << endl;
+	}
+	~X(){
+		cout << "X类的析构函数被调用" << endl;
+	}
+
+public:
+	void functest(){
+		cout << "functest()成员函数被调用" << endl;
+	}
+
+};
+
+//void func(X tmpx)
+//void func(X &tmpx)
+//{
+//	return;
+//}
+
+X func() {
+	X x0;
+	//......
+	return x0;
+}
+
+void func(X& extra) { //1：安插一个引用参数
+	X x0;  //这个在我们看来之所以能调用构造函数，是因为编译器内部有诸如x0.X::X()这种代码
+	//..
+	extra.X::X(x0);  //2：return之前安插一个对拷贝构造函数的调用
+	//return x0;
+	return;
+}
+
+
+int main(){
+	//{
+	//	X x0;
+	//	x0.m_i = 15;
+	//	X x1 = x0;
+	//	X x2(x0);
+	//	X x3 = x0;
+
+
+	//	//步骤一：定义一个对象，为对象分配内存，但这里编译器内部并没有调用构造函数。如果以编译器的眼光看X x3_2，则不会调用构造函数。除非编译器主动增加这种x3_2.X::X();这种代码行到现有的代码中
+	//	X x3_2;
+	//	//步骤二：直接调用对象的拷贝构造函数去了
+	//	x3_2.X::X(x0);
+
+	//}
+
+	//{
+	//	X x0;
+	//	//func(x0); //程序员视角（现代编译器视角）
+
+	//	//X x0;
+	//	//X tmpx = x0;  
+
+	//	//老编译器视角
+	//	X tmpobj;  //编译器产生出来一个临时对象
+	//	tmpobj.X::X(x0); //调用拷贝构造函数
+	//	func(tmpobj); //用临时对象调用func
+	//	tmpobj.X::~X(); //func()被调用完成后，析构函数被调用	
+	//}
+	{
+		X my = func();
+		cout << "断点设置在这里" << endl;
+	}
+	{
+		//编译器视角
+		X my; //从编译器的视角看，这里是不调用构造函数的,除非编译器主动增加代码my.X::X();
+		func(my);
+	}
+
+	{
+		func().functest();
+	}
+	{
+		X my;
+		(func(my), my).functest(); //逗号表达式，先求解表达式1，再求解表达式2，整个表达式的值是表达式2的值
+	}
+
+	{
+		X(*pf)();   //定义函数指针
+		pf = func;
+		pf().functest();
+	}
+	{
+		X my;
+		void (*pf)(X&);
+		pf = func;
+		pf(my);
+		my.functest();
+
+	}
+	return 0;
+}
