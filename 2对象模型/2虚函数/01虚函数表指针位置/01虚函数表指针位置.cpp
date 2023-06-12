7ec8240f2893994238425146df8f16c7bcc2f5e7
xlin <linxianghs@163.com>
2023-06-12 15:23:19 +0800

    虚函数

---
+++ 2对象模型/2虚函数/01虚函数表指针位置/01虚函数表指针位置.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+//虚函数指针位于对象内存的开头
+
+class A
+{
+public:
+	int i;   //一个成员变量，4字节
+	virtual void testfunc() {}  //虚函数
+};
+
+
+int main() {
+
+	A a;
+	int ilen = sizeof(a);
+	//cout << ilen << endl;  16
+
+	return 0;
+}
