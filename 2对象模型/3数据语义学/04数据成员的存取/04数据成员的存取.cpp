5220fb8527086227e1f5549ed240ae9e5d3e735e
xlin <linxianghs@163.com>
2023-06-13 17:25:36 +0800

    数据

---
+++ 2对象模型/3数据语义学/04数据成员的存取/04数据成员的存取.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdio>
+
+using namespace std;
+
+class FAC {
+public:
+	int m_fai;
+	int m_faj;
+};
+
+//class MYACLS 
+class MYACLS : public FAC
+{
+public:
+	int m_i;
+	static int m_si; //这是声明不是定义
+	int m_j;
+
+public:
+	void myfunc()
+	{
+		m_i = 5;
+		m_j = 6;
+	}
+};
+int MYACLS::m_si = 0; //这才是定义
+
+
+int main() {
+
+	MYACLS myobj;
+	MYACLS* pmyobj = new MYACLS();
+	cout << MYACLS::m_si << endl;  //通过类名使用静态成员变量
+	cout << myobj.m_si << endl;    //通过对象名使用静态成员变量
+	cout << pmyobj->m_si << endl;  //对象指针
+
+	MYACLS::m_si = 1;
+	myobj.m_si = 2;
+	pmyobj->m_si = 3;
+
+	printf("myobj.m_i = %p\n", &myobj.m_i);
+	printf("pmyobj->m_i = %p\n", &pmyobj->m_i);
+	printf("MYACLS::m_si = %p\n", &MYACLS::m_si);
+	printf("MYACLS::m_si = %p\n", &myobj.m_si);
+	printf("MYACLS::m_si = %p\n", &pmyobj->m_si);
+
+	pmyobj->myfunc();
+
+	printf("MYACLS::m_i = %d\n", &MYACLS::m_i); //0
+	printf("MYACLS::m_j = %d\n", &MYACLS::m_j); //4
+
+	pmyobj->m_faj = 7;  //给父类的成员变量赋值
+
+	return 0;
+}
---
+++ 2对象模型/3数据语义学/05单一继承下的数据成员布局/05单一继承下的数据成员布局.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdio>
+
+using namespace std;
+
+//父类成员是先出现的，然后才是子类成员（这里是讨论常规状态）
+//class FAC  //父类
+//{
+//public:
+//	int m_fai;
+//	int m_faj;
+//};
+//class MYACLS : public FAC  //子类
+//{
+//public:
+//	int m_i;
+//	int m_j;
+//};
+
+//class Base
+//{
+//public:
+//	int m_i1;
+//	char m_c1;
+//	char m_c2;
+//	char m_c3;
+//};
+
+class Base1{
+
+public:
+	int m_i1;
+	char m_c1;
+};
+
+class Base2 :public Base1{
+
+public:
+	char m_c2;
+};
+
+class Base3 :public Base2{
+
+public:
+	char m_c3;
+};
+
+
+int main(){
+	/*{
+		printf("MYACLS::m_i = %d\n", &FAC::m_fai);
+		printf("MYACLS::m_i = %d\n", &FAC::m_faj);
+		printf("MYACLS::m_i = %d\n", &MYACLS::m_fai);
+		printf("MYACLS::m_i = %d\n", &MYACLS::m_faj);
+		printf("MYACLS::m_i = %d\n", &MYACLS::m_i);
+		printf("MYACLS::m_j = %d\n", &MYACLS::m_j);
+
+	}
+
+	{
+		FAC facobj;
+		MYACLS myaclobj;
+	}*/
+
+	/*{
+		cout << sizeof(Base) << endl;
+
+		printf("Base::m_i1 = %d\n", &Base::m_i1);
+		printf("Base::m_c1 = %d\n", &Base::m_c1);
+		printf("Base::m_c2 = %d\n", &Base::m_c2);
+		printf("Base::m_c3 = %d\n", &Base::m_c3);
+
+	}*/
+
+
+	cout << sizeof(Base1) << endl; //8
+	cout << sizeof(Base2) << endl; //12
+	cout << sizeof(Base3) << endl; //16
+
+	printf("Base3::m_i1 = %d\n", &Base3::m_i1);
+	printf("Base3::m_c1 = %d\n", &Base3::m_c1);
+	printf("Base3::m_c2 = %d\n", &Base3::m_c2);
+	printf("Base3::m_c3 = %d\n", &Base3::m_c3);
+
+
+	Base3 aaa;
+	aaa.m_c2 = 5;
+	return 0;
+}
