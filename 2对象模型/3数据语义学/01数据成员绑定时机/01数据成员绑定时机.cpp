a757ef83a47d8e34343eadef7a02e751e56a2ab0
xlin <linxianghs@163.com>
2023-06-13 11:02:57 +0800

    data

---
+++ 2对象模型/3数据语义学/01数据成员绑定时机/01数据成员绑定时机.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+//对成员函数myfunc的解析是整个类A定义完毕后才开始
+
+/*
+string myvar;
+class A {
+public:
+	int myfunc() {
+
+	}
+private:
+	int myvar;
+
+};
+*/
+
+typedef string mytype;
+class A {
+	typedef int mytype;
+public:
+	void myfunc(mytype tmpvalue);
+	//void myfunc(mytype tmpvalue) //这个mytype是string类型
+	//{
+	//	m_value = tmpvalue;
+	//}
+
+private:
+	//typedef int mytype;
+	mytype m_value;	//这个mytype是int类型	
+};
+void A::myfunc(mytype tmpvalue) //这个mytype又变成了int类型，但若在类中定义myfunc，那么参数中出现的mytype是string类型
+{
+	m_value = tmpvalue;
+}
+
+void myfunc(mytype tmpvalue)//这个mytype就是string类型
+{
+	string mvalue = tmpvalue;
+}
+
+int main() {
+
+
+
+	return 0;
+}
