c27e4a3a8754332bdca7e88acc18c9686bcd033f
xlin <linxianghs@163.com>
2023-06-13 22:02:02 +0800

    数据语义

---
+++ 2对象模型/3数据语义学/06单类单继承虚函数下的数据成员布局/06单类单继承虚函数下的数据成员布局.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cstdio>
+
+using namespace std;
+
+class Base{
+public:
+	//virtual  void mybvirfunc() {}
+	int m_bi;
+};
+
+class MYACLS :public Base{
+public:
+	int m_i;
+	int m_j;
+
+	virtual void myvirfunc(){
+	}
+	MYACLS() {//构造函数
+		int abc = 1; //方便加断点
+	}
+	~MYACLS(){//析构函数
+		int def = 0; //方便加断点
+	}
+};
+
+int main() {
+	/*
+	MYACLS aaa;
+	cout << sizeof(MYACLS) << endl;
+	printf("MYACLS::m_i = %d\n", &MYACLS::m_i);
+	printf("MYACLS::m_j = %d\n", &MYACLS::m_j);
+	*/
+
+	//MYACLS myobj;
+	//myobj.m_i = 3;
+	//myobj.m_j = 6;
+	//cout << "方便加断点" << endl;
+
+	MYACLS myobj;
+	myobj.m_i = 3;
+	myobj.m_j = 6;
+	myobj.m_bi = 9;
+	cout << "方便加断点" << endl;
+
+	return 0;
+}
