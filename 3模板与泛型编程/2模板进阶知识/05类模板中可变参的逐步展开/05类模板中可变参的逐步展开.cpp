073ce5d7dd718a7fea64b249f90e324743527143
xxlinx <linxianghs@163.com>
2023-07-08 23:08:24 +0800

    可变参展开

---
+++ 3模板与泛型编程/2模板进阶知识/05类模板中可变参的逐步展开/05类模板中可变参的逐步展开.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+#include <boost/type_index.hpp>
+
+#pragma warning(disable : 4996) 
+
+namespace _nmsp1 {
+	template<typename... Args>
+	class myclasst {
+	public:
+		myclasst() {
+			cout << "myclasst::myclasst()执行了，可变参个数=" << sizeof...(Args) << endl;
+		}
+	};
+
+	template<typename... Args>
+	//class myclasst2 : public myclasst<Args...>
+	//class myclasst2 : public myclasst <Args>...
+	//class myclasst2 : public myclasst <Args,char>...
+	class myclasst2 : public myclasst <Args, Args...>... {
+	public:
+		myclasst2() {
+			cout << "myclasst2::myclasst2()执行了，可变参个数=" << sizeof...(Args) << endl;
+		}
+	};
+
+}
+
+int main() {
+	_nmsp1::myclasst2<double, float, int> tmpobj;
+
+	return 0;
+}
